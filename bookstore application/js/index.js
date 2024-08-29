0e63f8878e571474c891e3d3ae9d8c211bb0a755
Joseph Krohn <104330537+joseph-krohn@users.noreply.github.com>
2024-08-29 17:21:14 +0900

    Delete bookstore application directory

--- bookstore application/js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const searchBar = document.querySelector('header input[type="text"]');
-    const searchButton = document.querySelector('header button');
-
-    searchButton.addEventListener('click', () => {
-        const searchTerm = searchBar.value;
-        window.location.href = `search.html?query=${searchTerm}`;
-    });
-});
